43cc9fcf866675d86c7dccc744b0d8858ce80aa7
danielchih1021 <chi17016@byui.edu>
2020-09-19 00:55:23 -0400

    Fifth Commit

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', (event)=>{
     const lu = document.querySelector('#lastupdated');
-    lu.textcontent = document.lastModified;
+    lu.textContent = document.lastModified;
 
     const cry = document.querySelector("#copyrightyear");
-    cry.textcontent = new Date().getFullYear();
+    cry.textContent = new Date().getFullYear();
 })
