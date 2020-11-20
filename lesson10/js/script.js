3c6d1ec7c1002659546e605ff2b33af995c05fb5
danielchih1021 <chi17016@byui.edu>
2020-11-19 21:22:19 -0500

    commit

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -30,7 +30,7 @@ window.addEventListener('load',(event)=>{
       */
     
     //Pull info via API from the weather website
-    const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=bf7358fc6ab851b5938eef8f5f7118f1';
+    const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=bf7358fc6ab851b5938eef8f5f7118f1';
 
     fetch(apiURL)
     .then((response) => response.json())
@@ -39,8 +39,50 @@ window.addEventListener('load',(event)=>{
       document.getElementById('desc').innerHTML = jsObject.weather[0]['description'];
       document.getElementById('temp').innerHTML = jsObject.main.temp;
       document.getElementById('humid').innerHTML = jsObject.main.humidity;
-      document.getElementById('windSpeed').innerHTML = jsObject.wind[0];
+      document.getElementById('windSpeed').innerHTML = jsObject.wind.speed;
     });
- 
+
+    const apifcURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=bf7358fc6ab851b5938eef8f5f7118f1';
+    fetch(apifcURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+      console.log(jsObject);
+      const listLength = jsObject.list.length
+      console.log(listLength);
+      function getNextListCount(listLength){
+        const listNumArray = [];
+        for(i=0;i<listLength;i++){
+          if (jsObject.list[i].dt_txt.substr(2, 8) = "18:00:00") {
+            listNumArray.push(i);
+          }
+        }
+        return listNumArray;
+      }  
+      const countArray = getNextListCount(listLength);
+      console.log(countArray);
+      /*
+        var d = new Date();
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        ---------var n = weekday[d.getDay()];
+
+        for(j=0;j<countArray.length;j++){  
+          const wdayWithNum = 'wday' + j;
+          document.getElementById(wdayWithNum) = weekday[d.getDay()+j];
+          const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[j].icon + '.png';
+          const desc = jsObject.weather[j].description;  // note how we reference the weather array
+          const iconWithNum = 'icon' + j;
+          document.getElementById(iconWithNum).setAttribute('src', imagesrc);  // focus on the setAttribute() method
+          document.getElementById('icon').setAttribute('alt', desc);
+        } 
+      */
+    });
   
 });
