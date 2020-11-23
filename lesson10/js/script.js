23475491eb98a3efb881c7a7a7d9823e5c4ceea6
danielchih1021 <chi17016@byui.edu>
2020-11-23 13:00:54 -0500

    commit

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -42,48 +42,42 @@ window.addEventListener('load',(event)=>{
       document.getElementById('windSpeed').innerHTML = jsObject.wind.speed;
     });
 
-    const apifcURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=bf7358fc6ab851b5938eef8f5f7118f1';
-    fetch(apifcURL)
+    const forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=bf7358fc6ab851b5938eef8f5f7118f1';
+    fetch(forecastURL)
     .then((response) => response.json())
     .then((jsObject) => {
       console.log(jsObject);
       const listLength = jsObject.list.length
       console.log(listLength);
-      console.log(jsObject.list[0].dt_txt.substr(2, 8));
-      function getNextListCount(listLength){
-        const listNumArray = [];
-        for(i=0;i<listLength;i++){
-          if (jsObject.list[i].dt_txt.substr(2, 8) = "18:00:00") {
-            listNumArray.push(i);
+      const stations = jsObject;
+      var elementCount = 0
+      for (i = 0; i < stations.list.length; i++) {
+        if (stations.list[i].dt_txt.includes("18:00:00")) {
+          var days = ['Sunday', 'Monday', 'Tuesday',
+           'Wednesday', 'Thursday', 'Friday', 'Saturday'
+          ];
+          var month = stations.list[i].dt_txt.substr(5, 2);
+          var date = stations.list[i].dt_txt.substr(8, 2);
+          var d = new Date(2020, month, date);
+          if (d.getDay() + 5 > 6) {
+            var newD = d.getDay() + 5 - 7;
+          } 
+          else {
+            var newD = d.getDay() + 5;
           }
+
+          const wdayAndNum = 'wday' + elementCount;
+          const iconAndNum = 'icon' + elementCount;
+          const tempAndNum = 'temp' + elementCount;
+
+          document.getElementById(tempAndNum).textContent = Math.round(stations.list[i].main.temp_max) + "°";
+          document.getElementById(wdayAndNum).textContent = days[newD];
+
+          document.getElementById(iconAndNum).setAttribute('src', 'https://openweathermap.org/img/w/' + stations.list[i].weather[0].icon + '.png');
+          document.getElementById(iconAndNum).setAttribute('alt', stations.list[i].weather[0].icon + '.png');
+
+          elementCount = elementCount + 1;
         }
-        return listNumArray;
-      }  
-      const countArray = getNextListCount(listLength);
-      console.log(countArray);
-      /*
-        var d = new Date();
-        var weekday = new Array(7);
-        weekday[0] = "Sunday";
-        weekday[1] = "Monday";
-        weekday[2] = "Tuesday";
-        weekday[3] = "Wednesday";
-        weekday[4] = "Thursday";
-        weekday[5] = "Friday";
-        weekday[6] = "Saturday";
-
-        ---------var n = weekday[d.getDay()];
-
-        for(j=0;j<countArray.length;j++){  
-          const wdayWithNum = 'wday' + j;
-          document.getElementById(wdayWithNum) = weekday[d.getDay()+j];
-          const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[j].icon + '.png';
-          const desc = jsObject.weather[j].description;  // note how we reference the weather array
-          const iconWithNum = 'icon' + j;
-          document.getElementById(iconWithNum).setAttribute('src', imagesrc);  // focus on the setAttribute() method
-          document.getElementById('icon').setAttribute('alt', desc);
-        } 
-      */
+      }
     });
-  
 });
