5cb4bcba8a070368c12599941587151ce2852002
danielchih1021 <chi17016@byui.edu>
2020-11-24 00:03:43 -0500

    commit

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -6,12 +6,13 @@ fetch(requestURL)
     })
     .then(function (jsonObject){
         console.table(jsonObject); //temporary checking for valid response and data parsing
-        const towns = jsonObject;
+        const towns = jsonObject['towns'];
         for(let i = 0; 1 < towns.length; i++) {
             let motto = document.createElement('p');
             let years = document.createElement('p');
             let population = document.createElement('p');
             let avgRF = document.createElement('p');
+            let card = document.createElement('section');
             if(towns[i].name.includes("Preston")){
 
                 motto.textContent = towns[i].motto;
@@ -23,12 +24,12 @@ fetch(requestURL)
                 population.classname="prestonInfo";
                 avgRF.textContent = "Average Rainfall: " + towns[i].averageRainfall;
                 avgRF.classname="prestonInfo";
-                var card = document.querySelector('div.sub-catP');
 
                 card.appendChild(motto);
                 card.appendChild(years);
                 card.appendChild(population);
                 card.appendChild(avgRF);
+                document.querySelector('div.sub-catP').appendChild(card);
             } else if (towns[i].name.includes("Fish Haven")){
                 motto.textContent = towns[i].motto;
                 motto.id="FHMotto";
@@ -39,12 +40,12 @@ fetch(requestURL)
                 population.classname="FHInfo";
                 avgRF.textContent = "Average Rainfall: " + towns[i].averageRainfall;
                 avgRF.classname="FHInfo";
-                var card = document.querySelector('div.sub-catFH');
-
+                
                 card.appendChild(motto);
                 card.appendChild(years);
                 card.appendChild(population);
-                card.appendChild(avgRF)
+                card.appendChild(avgRF);
+                document.querySelector('div.sub-catFH').appendChild(card);
             } else if (towns[i].name.includes("Soda Spring")){
                 motto.textContent = towns[i].motto;
                 motto.id="SSMotto";
@@ -54,12 +55,11 @@ fetch(requestURL)
                 population.textContent = "Current Population: " + towns[i].currentPopulation;
                 population.classname="SSInfo";
                 avgRF.textContent = "Average Rainfall: " + towns[i].averageRainfall;
-                var card = document.querySelector('div.sub-catSS');
-
                 card.appendChild(motto);
                 card.appendChild(years);
                 card.appendChild(population);
-                card.appendChild(avgRF)
+                card.appendChild(avgRF);
+                document.querySelector('div.sub-catSS').appendChild(card);
             }
         }
     });
