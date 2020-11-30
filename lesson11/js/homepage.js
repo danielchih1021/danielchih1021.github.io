42406e0487658f9883ff9ebd195a9734c3b79ed0
danielchih1021 <chi17016@byui.edu>
2020-11-30 16:42:10 -0500

    commit

--- lesson11/js/homepage.js
+++ lesson11/js/homepage.js
@@ -7,7 +7,7 @@ fetch(requestURL)
     .then(function (jsonObject){
         console.table(jsonObject); //temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
-        for(let i = 0; 1 < towns.length; i++) {
+        for(let i = 0; i < towns.length; i++) {
             let motto = document.createElement('p');
             let years = document.createElement('p');
             let population = document.createElement('p');
